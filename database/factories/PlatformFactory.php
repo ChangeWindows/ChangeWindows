13cfe39ed8ad2e94097b1246ea3e759c885927bf
Yannick <yannick.franssen@outlook.com>
2021-03-18 23:14:33 +0100

    #5 Update factory with legacy field

--- database/factories/PlatformFactory.php
+++ database/factories/PlatformFactory.php
@@ -26,7 +26,8 @@ class PlatformFactory extends Factory
             'color' => $this->faker->hexColor(),
             'icon' => $this->faker->randomElement(['laptop', 'mobile', 'gamepad-modern', 'server', 'head-side-goggles', 'microchip', 'tv', 'compact-disc', 'code', 'tablet', 'cloud']),
             'position' => $this->faker->randomNumber(5),
-            'active' => 1
+            'active' => 1,
+            'legacy' => 0
         ];
     }
 }
