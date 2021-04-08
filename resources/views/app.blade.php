7403c6c9155550048dbd46fc98b8a5b275b2b756
Yannick <yannick.franssen@outlook.com>
2021-04-08 20:51:11 +0200

    Temporary favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="{{ asset('js/bootstrap.bundle.min.js') }}" defer></script>
+        
+        <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     </head>
     <body>
         @inertia
