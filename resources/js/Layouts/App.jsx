81c63b0c7485792c9a663bc4b938dfbfa8bf8bf5
Yannick <yannick.franssen@outlook.com>
2021-04-08 20:11:28 +0200

    Update about design

--- resources/js/Layouts/App.jsx
+++ resources/js/Layouts/App.jsx
@@ -1,15 +1,16 @@
+import clsx from 'clsx'
 import React from 'react'
 
 import Navigation from './Navigation/Navigation'
 
-export default function App({ children, can }) {
+export default function App({ children, can, background = false }) {
     return (
         <div className="bg-light">
             <div className="grid">
                 <header className="grid-sidebar">
                     <Navigation can={can} />
                 </header>
-                <main className="grid-content">
+                <main className={clsx('grid-content', { 'auth': background })}>
                     { children }
                 </main>
             </div>
--- resources/js/Pages/About/Show.jsx
+++ resources/js/Pages/About/Show.jsx
@@ -8,23 +8,15 @@ import { faSunHaze } from '@fortawesome/pro-regular-svg-icons';
 
 export default function Show({ can, app }) {
     return (
-        <App can={can}>
-            <div className="container-fluid">
-                <div className="row vh-100 justify-content-center align-items-center">
-                    <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h1 className="h2 mb-0 text-primary"><FontAwesomeIcon icon={faSunHaze} fixedWidth /> About Horizon</h1>
-                                <p className="text-muted">Version {app.version}</p>
-                                <a href="https://twitter.com/changewindows" target="_blank" className="btn btn-primary btn-sm me-1 mb-3"><FontAwesomeIcon icon={faTwitter} /> Twitter</a>
-                                <a href="https://github.com/changewindows/horizon" target="_blank" className="btn btn-primary btn-sm me-1 mb-3"><FontAwesomeIcon icon={faGithub} /> GitHub</a>
-                                <p>ChangeWindows is a detailed changelog and release history for Windows across all platforms it appears on. With detailed and timely updates, as well as a clean and clear interface, ChangeWindows' goal is to provide a solid resource for anyone interested in knowing what's next for Windows.</p>
-                                <p>&copy; 2014-2021 &middot; All Right Reserved</p>
-                                <p className="m-0 h4 f-384">Studio <span className="studio-384">384</span></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <App can={can} background>
+            <div className="auth-card auth-card-lg">
+                <h1 className="h3 m-0 pt-5 text-center"><FontAwesomeIcon icon={faSunHaze} fixedWidth /> About Horizon</h1>
+                <p className="text-muted pb-5 m-0 text-center">Version {app.version}</p>
+                <a href="https://twitter.com/changewindows" target="_blank" className="btn btn-primary btn-sm me-1 mb-3"><FontAwesomeIcon icon={faTwitter} /> Twitter</a>
+                <a href="https://github.com/changewindows/horizon" target="_blank" className="btn btn-primary btn-sm me-1 mb-3"><FontAwesomeIcon icon={faGithub} /> GitHub</a>
+                <p>ChangeWindows is a detailed changelog and release history for Windows across all platforms it appears on. With detailed and timely updates, as well as a clean and clear interface, ChangeWindows' goal is to provide a solid resource for anyone interested in knowing what's next for Windows.</p>
+                <p>&copy; 2014-2021 &middot; All Right Reserved</p>
+                <p className="m-0 h4 f-384">Studio <span className="studio-384">384</span></p>
             </div>
         </App>
     )
