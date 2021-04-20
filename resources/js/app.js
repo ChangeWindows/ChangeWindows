247593b1948004ae57ebba90e3d48116e1f8b4a0
Yannick <yannick.franssen@outlook.com>
2021-04-20 20:55:19 +0200

    Update progress indicator color

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,15 @@ require('./bootstrap');
 
 require('alpinejs');
 
-import { App } from '@inertiajs/inertia-react'
-import React from 'react'
-import { render } from 'react-dom'
-import { InertiaProgress } from '@inertiajs/progress'
+import React from 'react';
+import { render } from 'react-dom';
+import { InertiaProgress } from '@inertiajs/progress';
+import { App } from '@inertiajs/inertia-react';
 
-const el = document.getElementById('app')
-InertiaProgress.init()
+const el = document.getElementById('app');
+InertiaProgress.init({
+    color: '#0066ff'
+});
 
 render(
     <App
