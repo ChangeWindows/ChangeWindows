c34473e3c1cb13f7154e0b8de806153ce1a27652
Yannick <yannick.franssen@outlook.com>
2021-04-20 20:55:11 +0200

    Update navigation design, make it slightly smaller

--- resources/js/Layouts/Navigation/components/NavigationBar.jsx
+++ resources/js/Layouts/Navigation/components/NavigationBar.jsx
@@ -22,7 +22,7 @@ export default function NavigationBar({ auth, items }) {
 	const [showAuthOnMainBar, setShowAuthOnMainBar] = useState(true);
 
 	const [mainItems, overflowItems] = useMemo(() => {
-		const maxVisibleItems = Math.floor(width / 80);
+		const maxVisibleItems = Math.floor(width / 60);
 
 		if (items.length > maxVisibleItems && !matchesSmUp) {
 			items = items.filter((item) => item.type !== 'divider' && !item.ignore && (item.permission === true || item.permission === undefined));
