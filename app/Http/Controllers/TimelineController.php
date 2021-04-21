3ffc28c7362fc5d42c8ebca9dbecea77505110d4
Yannick <yannick.franssen@outlook.com>
2021-04-21 22:45:11 +0200

    #40 Start implementing platform timelines for home

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -94,8 +94,10 @@ class TimelineController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(Platform $platform)
     {
+        $channel_platforms = Platform::orderBy('tool')->orderBy('position')->where('active', '=', '1')->get();
+
         return Inertia::render('Timeline/Show', [
             'platforms' => Platform::orderBy('position')->get()->map(function ($_platform) {
                 return [
@@ -107,7 +109,64 @@ class TimelineController extends Controller
                     'tool' => $_platform->tool,
                     'url' => route('front.timeline.show', $_platform, false)
                 ];
-            })
+            }),
+            'channel_platforms' => $channel_platforms->map(function ($platform) {
+                return [
+                    'name' => $platform->name,
+                    'color' => $platform->color,
+                    'icon' => $platform->icon,
+                    'channels' => $platform->channels->where('active')->map(function ($channel) {
+                        $release_channels = $channel->releaseChannels
+                            ->sortByDesc(function ($release_channel, $key) {
+                                return $release_channel->release->canonical_version;
+                            })->values()->all();
+        
+                        return [
+                            'name' => $release_channels[0]->short_name,
+                            'order' => $channel->order,
+                            'color' => $channel->color,
+                            'flights' => collect($release_channels)->map(function ($_channel) {
+                                if (count($_channel->flights) > 0) {
+                                    return [
+                                        'version' => $_channel->flights[0]->flight,
+                                        'date' => $_channel->flights[0]->timeline->date
+                                    ];
+                                }
+                            })->where('version', '<>', null)->values()->all()
+                        ];
+                    })->sortBy('order')->values()->all(),
+                ];
+            }),
+            'timeline' => $platform->timeline->sortByDesc('date')->groupBy('date')->map(function ($items, $date) {
+                return [
+                    'date' => $items[0]->date,
+                    'flights' => $items->groupBy(function($item, $key) {
+                        return $item->entry->flight.'-'.$item->entry->platform->position;
+                    })->map(function ($flights) {
+                        $_cur_flight = $flights->first();
+
+                        return [
+                            'id' => $_cur_flight->entry->id,
+                            'flight' => $_cur_flight->entry->flight,
+                            'date' => $_cur_flight->entry->timeline->date,
+                            'version' => $_cur_flight->entry->releaseChannel->release->version,
+                            'release_channel' => $flights->map(function ($channels) {
+                                return [
+                                    'name' => $channels->entry->releaseChannel->short_name,
+                                    'color' => $channels->entry->releaseChannel->channel->color
+                                ];
+                            }),
+                            'platform' => [
+                                'icon' => $_cur_flight->entry->platform->icon,
+                                'name' => $_cur_flight->entry->platform->name,
+                                'color' => $_cur_flight->entry->platform->color
+                            ],
+                            'edit_url' => $_cur_flight->entry->edit_url
+                        ];
+                    })->values()->all()
+                ];
+            }),
+            'status' => session('status')
         ]);
     }
 }
--- resources/js/Pages/Timeline/Show.jsx
+++ resources/js/Pages/Timeline/Show.jsx
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import App from '../../Layouts/App';
+import Channel from '../../Components/Cards/Channel';
+import Flight from '../../Components/Timeline/Flight';
+import Timeline from '../../Components/Timeline/Timeline';
 
+import PlatformIcon from '../../Components/Platforms/PlatformIcon';
 import PlatformNavigation from '../../Components/PlatformNavigation';
 
-export default function Show({ can, auth, platforms }) {
+import { format, parseISO } from 'date-fns';
+import clsx from 'clsx';
+
+export default function Show({ can, auth, timeline, platforms, channel_platforms }) {
     return (
         <App can={can} auth={auth}>
             <PlatformNavigation all="/timeline" page="Timeline" platforms={platforms} />
         
             <div className="container my-3">
                 <div className="row g-3">
-                    <div className="col-12 mt-4">
-                        <h2 className="h5 mb-3 fw-bold">Nothing to see here yet</h2>
-                        <p>We're still working on this... See you again later.</p>
+                    <div className="col-12 col-md-8 col-lg-7">
+                        <div className="row g-4">
+                            {Object.keys(timeline).map((date, key) => (
+                                <Timeline date={format(parseISO(timeline[date].date), 'd MMMM yyyy')} key={key}>
+                                    {timeline[date].flights.map((flight, _key) => (
+                                        <Flight
+                                            key={_key}
+                                            platform={flight.platform}
+                                            build={flight.flight}
+                                            channels={flight.release_channel}
+                                            version={flight.version}
+                                        />
+                                    ))}
+                                </Timeline>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="d-none d-md-block col-md-4 col-lg-5">
+                        <div className="row g-2">
+                            {channel_platforms.map((platform, key) => (
+                                <Fragment key={key}>
+                                    <div className={clsx('col-12 mb-n1', { 'mt-3': key > 0 })}>
+                                        <h3 className="h6" style={{ color: platform.color }}>
+                                            <PlatformIcon platform={platform} color />
+                                            <span className="fw-bold ms-2">{platform.name}</span>
+                                        </h3>
+                                    </div>
+                                    {platform.channels.map((channel, _key) => (
+                                        <Channel
+                                            key={_key}
+                                            channel={{ color: channel.color, name: channel.name }}
+                                            build={channel.flights.length > 0 ? channel.flights[0].version : ''}
+                                            date={channel.flights.length > 0 ? format(parseISO(channel.flights[0].date), 'd MMMM yyyy') : ''}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
