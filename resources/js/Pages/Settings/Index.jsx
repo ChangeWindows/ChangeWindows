d6199679bbc7be7cb65d97ffb15e74b2ee4670a5
Yannick <yannick.franssen@outlook.com>
2021-05-20 22:07:01 +0200

    #106 Fix various errors in markup for Settings page

--- resources/js/Pages/Settings/Index.jsx
+++ resources/js/Pages/Settings/Index.jsx
@@ -40,21 +40,21 @@ export default function Show() {
                 <div className="row g-3">
                     <div className="col-12 mt-4">
                         <h2 className="h5 mb-3 fw-bold">Theme</h2>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="theme" id="system" checked={theme === 'default'} onClick={() => toggle('default')} />
-                                <label class="form-check-label" for="system">
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="theme" id="system" checked={theme === 'default'} onChange={() => toggle('default')} />
+                                <label className="form-check-label" htmlFor="system">
                                 System
                             </label>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="theme" id="dark" checked={theme === 'dark'} onClick={() => toggle('dark')} />
-                                <label class="form-check-label" for="dark">
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="theme" id="dark" checked={theme === 'dark'} onChange={() => toggle('dark')} />
+                                <label className="form-check-label" htmlFor="dark">
                                 Dark
                             </label>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="theme" id="light" checked={theme === 'light'} onClick={() => toggle('light')} />
-                                <label class="form-check-label" for="light">
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="theme" id="light" checked={theme === 'light'} onChange={() => toggle('light')} />
+                                <label className="form-check-label" htmlFor="light">
                                 Light
                             </label>
                         </div>
