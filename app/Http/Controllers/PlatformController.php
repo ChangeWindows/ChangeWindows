a1d07a90efdfeee3a66f021ac9dcd4ab00f20f2d
Yannick <yannick.franssen@outlook.com>
2021-05-21 21:58:26 +0200

    #110 Remove timeline from platforms

--- app/Http/Controllers/PlatformController.php
+++ app/Http/Controllers/PlatformController.php
@@ -29,35 +29,6 @@ class PlatformController extends Controller
      */
     public function show(Platform $platform)
     {
-        $timeline = Timeline::orderBy('date', 'desc')
-            ->whereHas('flight', function (Builder $query) use ($platform) {
-                $query->join('release_channels as frs', function ($join) {
-                    $join->on('frs.id', '=', 'flights.release_channel_id')
-                
-                    ->join('channels as fc', function ($join) {
-                        $join->on('fc.id', '=', 'frs.channel_id');
-                    });
-                })
-                ->where('fc.platform_id', '=', $platform->id);
-            })
-            ->orWhereHas('promotion', function (Builder $query) use ($platform) {
-                $query->join('release_channels as prs', function ($join) {
-                    $join->on('prs.id', '=', 'promotions.release_channel_id')
-                
-                    ->join('channels as pc', function ($join) {
-                        $join->on('pc.id', '=', 'prs.channel_id');
-                    });
-                })
-                ->where('pc.platform_id', '=', $platform->id);
-            })
-            ->orWhereHas('launch', function (Builder $query) use ($platform) {
-                $query->join('releases as lr', function ($join) {
-                    $join->on('lr.id', '=', 'launches.release_id');
-                })
-                ->where('lr.platform_id', '=', $platform->id);
-            })
-            ->paginate(75);
-
         return Inertia::render('Platforms/Show', [
             'platforms' => Platform::where('tool', 0)->orderBy('position')->get()->map(function ($_platform) {
                 return [
@@ -134,102 +105,7 @@ class PlatformController extends Controller
                         ];
                     })->values()->all()
                 ];
-            })->values()->all(),
-            'timeline' => $timeline->sortByDesc('date')->groupBy('date')->map(function ($items, $date) {
-                return [
-                    'date' => $items[0]->date,
-                    'flights' => $items->groupBy(function($item, $key) {
-                        if ($item->item_type === \App\Models\Flight::class) {
-                            return $item->item->flight.'-'.$item->item->platform->position;
-                        } else if ($item->item_type === \App\Models\Promotion::class) {
-                            return $item->item->platform->position.'.'.$item->item->releaseChannel->release->version.'.'.$item->item->releaseChannel->channel->order;
-                        } else if ($item->item_type === \App\Models\Launch::class) {
-                            return $item->item->platform->position.'.'.$item->item->release->version;
-                        }
-                    })->map(function ($flights) {
-                        if ($flights->first()->item_type === \App\Models\Flight::class) {
-                            $_cur_flight = $flights->first();
-                            return [
-                                'type' => 'flight',
-                                'event_priority' => 3,
-                                'id' => $_cur_flight->item->id,
-                                'flight' => $_cur_flight->item->releaseChannel->release->package ? $_cur_flight->item->version : $_cur_flight->item->flight,
-                                'date' => $_cur_flight->item->timeline->date,
-                                'version' => $_cur_flight->item->releaseChannel->release->version,
-                                'cversion' => $_cur_flight->item->releaseChannel->release->canonical_version,
-                                'package' => $_cur_flight->item->releaseChannel->release->package ? $_cur_flight->item->releaseChannel->release->name : false,
-                                'release_channel' => $flights->map(function ($channels) {
-                                    return [
-                                        'order' => $channels->item->releaseChannel->channel->order,
-                                        'name' => $channels->item->releaseChannel->short_name,
-                                        'color' => $channels->item->releaseChannel->channel->color
-                                    ];
-                                })->sortBy('order')->values()->all(),
-                                'platform' => [
-                                    'order' => $_cur_flight->item->platform->order,
-                                    'icon' => $_cur_flight->item->platform->icon,
-                                    'name' => $_cur_flight->item->platform->name,
-                                    'tool' => $_cur_flight->item->platform->tool,
-                                    'color' => $_cur_flight->item->platform->color
-                                ],
-                                'url' => $_cur_flight->item->url
-                            ];
-                        }
-                        
-                        if ($flights->first()->item_type === \App\Models\Promotion::class) {
-                            $_cur_promotion = $flights->first();
-                            return [
-                                'type' => 'promotion',
-                                'event_priority' => 2,
-                                'id' => $_cur_promotion->item->id,
-                                'date' => $_cur_promotion->item->timeline->date,
-                                'version' => $_cur_promotion->item->releaseChannel->release->version,
-                                'cversion' => $_cur_promotion->item->releaseChannel->release->canonical_version,
-                                'release_channel' => [
-                                    'name' => $_cur_promotion->item->releaseChannel->short_name,
-                                    'color' => $_cur_promotion->item->releaseChannel->channel->color
-                                ],
-                                'platform' => [
-                                    'order' => $_cur_promotion->item->platform->order,
-                                    'icon' => $_cur_promotion->item->platform->icon,
-                                    'name' => $_cur_promotion->item->platform->name,
-                                    'color' => $_cur_promotion->item->platform->color
-                                ],
-                                'url' => $_cur_promotion->item->url
-                            ]; 
-                        }
-                        
-                        if ($flights->first()->item_type === \App\Models\Launch::class) {
-                            $_cur_launch = $flights->first();
-                            return [
-                                'type' => 'launch',
-                                'event_priority' => 1,
-                                'id' => $_cur_launch->item->id,
-                                'date' => $_cur_launch->item->timeline->date,
-                                'version' => $_cur_launch->item->release->version,
-                                'cversion' => $_cur_launch->item->release->canonical_version,
-                                'platform' => [
-                                    'order' => $_cur_launch->item->platform->order,
-                                    'icon' => $_cur_launch->item->platform->icon,
-                                    'name' => $_cur_launch->item->platform->name,
-                                    'color' => $_cur_launch->item->platform->color
-                                ],
-                                'url' => $_cur_launch->item->url
-                            ];
-                        }
-                    })->sortByDesc(function ($item, $key) {
-                        if ($item['type'] === 'flight') {
-                            return $item['event_priority'].'.'.$item['cversion'].'.'.$item['flight'].'.'.$item['platform']['order'];
-                        }
-                        
-                        return $item['event_priority'].'.'.$item['cversion'].'.'.$item['platform']['order'];
-                    })->values()->all()
-                ];
-            }),
-            'pagination' => [
-                'prev_page_url' => $timeline->previousPageUrl(),
-                'next_page_url' => $timeline->nextPageUrl()
-            ]
+            })->values()->all()
         ]);
     }
 }
--- resources/js/Pages/Platforms/Show.jsx
+++ resources/js/Pages/Platforms/Show.jsx
@@ -2,22 +2,14 @@ import React, { useMemo } from 'react';
 
 import App from '../../Layouts/App';
 import Channel from '../../Components/Cards/Channel';
-import Flight from '../../Components/Timeline/Flight';
-import Launch from '../../Components/Timeline/Launch';
-import Promotion from '../../Components/Timeline/Promotion';
 import ReleaseCard from '../../Components/Cards/ReleaseCard';
-import Timeline from '../../Components/Timeline/Timeline';
 
 import PlatformIcon from '../../Components/Platforms/PlatformIcon';
 import PlatformNavigation from '../../Components/PlatformNavigation';
-import Pagination from '../../Components/Pagination';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFlag, faBarsStaggered } from '@fortawesome/pro-regular-svg-icons';
 
 import { format, parseISO } from 'date-fns';
 
-export default function Show({ can, auth, platforms, platform, channels, releases, packages, timeline, pagination }) {
+export default function Show({ platforms, platform, channels, releases, packages }) {
     const [currentReleases, legacyReleases] = useMemo(() => {
         const currentReleases = releases.filter((release) => release.channels.length > 0);
         const legacyReleases = releases.filter((release) => release.channels.length === 0);
@@ -48,123 +40,61 @@ export default function Show({ can, auth, platforms, platform, channels, release
                         </div>
                     </div>
                     <div className="col-12">
-                        <nav className="mt-4">
-                            <div className="nav nav-lined" id="nav-tab" role="tablist">
-                                <button className="nav-link active" id="nav-releases-tab" data-bs-toggle="tab" data-bs-target="#nav-releases" type="button" role="tab" aria-controls="nav-releases" aria-selected="true"><FontAwesomeIcon icon={faFlag} fixedWidth /> Releases</button>
-                                <button className="nav-link" id="nav-timeline-tab" data-bs-toggle="tab" data-bs-target="#nav-timeline" type="button" role="tab" aria-controls="nav-timeline" aria-selected="false"><FontAwesomeIcon icon={faBarsStaggered} fixedWidth /> Timeline</button>
-                            </div>
-                        </nav>
-                        <div className="tab-content" id="nav-tabContent">
-                            <div className="tab-pane fade show active" id="nav-releases" role="tabpanel" aria-labelledby="nav-releases-tab">
-                                <div className="row">
-                                    {currentReleases.length > 0 &&
-                                        <div className="col-12 mt-4">
-                                            <h2 className="h5 mb-3 fw-bold">Current releases</h2>
-                                            <div className="row g-2">
-                                                {currentReleases.map((release, key) => {
-                                                    return (
-                                                        <ReleaseCard
-                                                            key={key}
-                                                            name={release.name}
-                                                            alts={[`Version ${release.version}`, release.codename]}
-                                                            channels={release.channels}
-                                                            url={release.url}
-                                                        />
-                                                    );
-                                                })}
-                                            </div>
-                                        </div>
-                                    }
-                                    {packages.length > 0 &&
-                                        <div className="col-12 mt-4">
-                                            <h2 className="h5 mb-3 fw-bold">Packages</h2>
-                                            <div className="row g-2">
-                                                {packages.map((pack, key) => {
-                                                    return (
-                                                        <ReleaseCard
-                                                            key={key}
-                                                            pack
-                                                            name={pack.name}
-                                                            alts={[`Version ${pack.version}`, pack.codename]}
-                                                            channels={pack.channels}
-                                                            url={pack.url}
-                                                        />
-                                                    );
-                                                })}
-                                            </div>
-                                        </div>
-                                    }
-                                    {legacyReleases.length > 0 &&
-                                        <div className="col-12 mt-4">
-                                            <h2 className="h5 mb-3 fw-bold">Unsupported releases</h2>
-                                            <div className="row g-2">
-                                                {legacyReleases.map((release, key) => {
-                                                    return (
-                                                        <ReleaseCard
-                                                            key={key}
-                                                            name={release.name}
-                                                            alts={[`Version ${release.version}`, release.codename]}
-                                                            url={release.url}
-                                                        />
-                                                    );
-                                                })}
-                                            </div>
-                                        </div>
-                                    }
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="nav-timeline" role="tabpanel" aria-labelledby="nav-timeline-tab">
-                                <div className="row">
-                                    <div className="col-12 mt-4">
-                                        <h2 className="h5 mb-3 fw-bold">Timeline</h2>
-                                        <div className="row g-4">
-                                            {Object.keys(timeline).map((date, key) => (
-                                                <Timeline date={format(parseISO(timeline[date].date), 'd MMMM yyyy')} key={key}>
-                                                    {timeline[date].flights.map((flight, _key) => {
-                                                        if (flight.type === 'flight') {
-                                                            return (
-                                                                <Flight
-                                                                    key={`${flight.type}-${flight.id}`}
-                                                                    platform={flight.platform}
-                                                                    build={flight.flight}
-                                                                    channels={flight.release_channel}
-                                                                    version={flight.version}
-                                                                    pack={flight.package}
-                                                                    url={flight.url}
-                                                                />
-                                                            );
-                                                        }
-                
-                                                        if (flight.type === 'promotion') {
-                                                            return (
-                                                                <Promotion
-                                                                    key={`${flight.type}-${flight.id}`}
-                                                                    platform={flight.platform}
-                                                                    channel={flight.release_channel}
-                                                                    version={flight.version}
-                                                                    url={flight.url}
-                                                                />
-                                                            );
-                                                        }
-                
-                                                        if (flight.type === 'launch') {
-                                                            return (
-                                                                <Launch
-                                                                    key={`${flight.type}-${flight.id}`}
-                                                                    platform={flight.platform}
-                                                                    version={flight.version}
-                                                                    url={flight.url}
-                                                                />
-                                                            );
-                                                        }
-                                                    })}
-                                                </Timeline>
-                                            ))}
-                                            <Pagination pagination={pagination} />
-                                        </div>
+                        <div className="row">
+                            {currentReleases.length > 0 &&
+                                <div className="col-12 mt-4">
+                                    <h2 className="h5 mb-3 fw-bold">Current releases</h2>
+                                    <div className="row g-2">
+                                        {currentReleases.map((release, key) => {
+                                            return (
+                                                <ReleaseCard
+                                                    key={key}
+                                                    name={release.name}
+                                                    alts={[`Version ${release.version}`, release.codename]}
+                                                    channels={release.channels}
+                                                    url={release.url}
+                                                />
+                                            );
+                                        })}
                                     </div>
                                 </div>
-                            </div>
+                            }
+                            {packages.length > 0 &&
+                                <div className="col-12 mt-4">
+                                    <h2 className="h5 mb-3 fw-bold">Packages</h2>
+                                    <div className="row g-2">
+                                        {packages.map((pack, key) => {
+                                            return (
+                                                <ReleaseCard
+                                                    key={key}
+                                                    pack
+                                                    name={pack.name}
+                                                    alts={[`Version ${pack.version}`, pack.codename]}
+                                                    channels={pack.channels}
+                                                    url={pack.url}
+                                                />
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            }
+                            {legacyReleases.length > 0 &&
+                                <div className="col-12 mt-4">
+                                    <h2 className="h5 mb-3 fw-bold">Unsupported releases</h2>
+                                    <div className="row g-2">
+                                        {legacyReleases.map((release, key) => {
+                                            return (
+                                                <ReleaseCard
+                                                    key={key}
+                                                    name={release.name}
+                                                    alts={[`Version ${release.version}`, release.codename]}
+                                                    url={release.url}
+                                                />
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            }
                         </div>
                     </div>
                 </div>
