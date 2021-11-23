d1645da5f3c38053aeae92a8c4ad93499fcc79b0
Yannick <yannick.franssen@outlook.com>
2021-11-23 20:48:03 +0100

    #140 Revamp channel page design

--- resources/js/Components/Cards/ReleaseCard.jsx
+++ resources/js/Components/Cards/ReleaseCard.jsx
@@ -3,31 +3,36 @@ import { InertiaLink } from '@inertiajs/inertia-react';
 
 import PlatformIcon from '../Platforms/PlatformIcon';
 
-import clsx from 'clsx';
-
 export default function ReleaseCard({ name, platform, channels, alts, url, pack = false }) {
-    const Component = useMemo(() => (url ? InertiaLink : 'div'), ['url']);
-    const mainProps = useMemo(() => ({ href: url }), ['url']);
+  const Component = useMemo(() => (url ? InertiaLink : 'div'), ['url']);
+  const mainProps = useMemo(() => ({ href: url }), ['url']);
 
-    return (
-        <div className="col-12 col-sm-6 col-xl-4 col-xxl-3">
-            <Component {...mainProps} className="card release">
-                <div className="d-flex flex-row">
-                    {platform && <h3 className="h6 mb-0"><PlatformIcon platform={platform} color /></h3>}
-                    <div className={clsx({ 'ms-2': platform })}>
-                        <h3 className="h6 mb-0">{name}</h3>
-                        {!pack && alts && !platform?.tool && <p className="text-muted mb-0 mt-n1"><small>{alts.join(', ')}</small></p>}
-                    </div>
+  return (
+    <div className="col-12">
+      <Component {...mainProps} className="card release">
+        <div className="d-flex flex-row">
+          {platform && <h3 className="h6 mb-0"><PlatformIcon platform={platform} color /></h3>}
+          <div className="ms-0 row flex-grow-1">
+            <div className="col-12 col-md-8">
+              <div className="row">
+                <div className="col-xl-8 col-12">
+                  <h3 className="h6 mb-0">{name}</h3>
                 </div>
-                <div className="flex-grow-1"></div>
-                {channels && <div className="release-channels mt-3">
-                    {channels.map((channel, key) => (
-                        <span key={key} className="badge me-1" style={{ backgroundColor: channel.color }}>
-                            {channel.short_name}
-                        </span>
-                    ))}
-                </div>}
-            </Component>
+                <div className="col-xl-4 col-12">
+                  {!pack && alts && !platform?.tool && <p className="text-muted mb-0 mt-n1"><small>{alts.join(', ')}</small></p>}
+                </div>
+              </div>
+            </div>
+            {channels && <div className="col-12 col-md-4 release-channels d-flex flex-row justify-content-start justify-content-md-end mt-2 mt-md-0 gap-1">
+              {channels.map((channel, key) => (
+                <span key={key} className="badge" style={{ backgroundColor: channel.color }}>
+                  {channel.short_name}
+                </span>
+              ))}
+            </div>}
+          </div>
         </div>
-    );
+      </Component>
+    </div>
+  );
 };
