1a56c81e541aad6d7d59e13272d4e1a0e62ee2d7
Yannick <yannick.franssen@outlook.com>
2021-11-24 22:36:54 +0100

    #138 Update theme detection logic

--- resources/js/Layouts/AppBar.jsx
+++ resources/js/Layouts/AppBar.jsx
@@ -14,14 +14,12 @@ export default function AppBar() {
   useEffect(() => {
     const theme = getLocal('theme');
 
-    if (theme === 'default') {
-      if (matchesDarkMode) {
-        document.head.children['color-scheme'].content = 'dark';
-        document.head.children['theme-color'].content = '#202020';
-      } else {
-        document.head.children['color-scheme'].content = 'light';
-        document.head.children['theme-color'].content = '#f3f3f3';
-      }
+    if (theme === 'dark' || theme === 'default' && matchesDarkMode) {
+      document.head.children['color-scheme'].content = 'dark';
+      document.head.children['theme-color'].content = '#202020';
+    } else {
+      document.head.children['color-scheme'].content = 'light';
+      document.head.children['theme-color'].content = '#f3f3f3';
     }
   }, [matchesDarkMode]);
 
@@ -33,13 +31,9 @@ export default function AppBar() {
     } else if (theme === 'light') {
       document.querySelector('html').classList.add('theme-light');
       document.querySelector('html').classList.remove('theme-default');
-      document.head.children['color-scheme'].content = 'light';
-      document.head.children['theme-color'].content = '#f3f3f3';
     } else if (theme === 'dark') {
       document.querySelector('html').classList.add('theme-dark');
       document.querySelector('html').classList.remove('theme-default');
-      document.head.children['color-scheme'].content = 'dark';
-      document.head.children['theme-color'].content = '#202020';
     }
   });
 
