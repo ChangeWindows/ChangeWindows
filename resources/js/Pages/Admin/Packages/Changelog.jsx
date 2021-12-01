b7162a0b7120256144fb2f887a3dab39bd446cf1
Yannick <yannick.franssen@outlook.com>
2021-12-01 21:14:29 +0100

    Fix changelog page being uneditable

--- resources/js/Pages/Admin/Packages/Changelog.jsx
+++ resources/js/Pages/Admin/Packages/Changelog.jsx
@@ -7,73 +7,70 @@ import NaviBar from '../../../Components/NaviBar';
 import AmaranthIcon, { aiCheck, aiFloppyDisc } from '@changewindows/amaranth';
 
 export default function Edit({ urls, release, status = null }) {
-    const [curRelease, setCurRelease] = useState({
-        name: '',
-        version: null,
-        canonical_version: null,
-        codename: '',
-        description: '',
-        changelog: '',
-        platform_id: null,
-        start_preview: null,
-        start_public: null,
-        start_extended: null,
-        start_lts: null,
-        end_lts: null,
-        start_build: null,
-        start_delta: null,
-        end_build: null,
-        end_delta: null
-    });
-    const [changelog, setChangelog] = useState(null);
+  const [curRelease, setCurRelease] = useState({
+    name: '',
+    version: null,
+    canonical_version: null,
+    codename: '',
+    description: '',
+    changelog: '',
+    platform_id: null,
+    start_preview: null,
+    start_public: null,
+    start_extended: null,
+    start_lts: null,
+    end_lts: null,
+    start_build: null,
+    start_delta: null,
+    end_build: null,
+    end_delta: null
+  });
 
-    useEffect(() => {
-        setCurRelease(release);
-    }, [release]);
+  useEffect(() => {
+    setCurRelease(release);
+  }, [release]);
 
-    function handleSubmit(event) {
-        const _release = Object.assign({}, curRelease);
-        _release.changelog = changelog;
-        event.preventDefault();
-        Inertia.patch(urls.update_release, _release);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    Inertia.patch(urls.update_release, curRelease);
+  }
 
-    return (
-        <Admin>
-            <form onSubmit={handleSubmit}>
-                <NaviBar
-                    back="/admin/packages"
-                    actions={
-                        <button className="btn btn-primary btn-sm" type="submit"><AmaranthIcon icon={aiFloppyDisc} /> Save</button>
-                    }
-                >
-                    {curRelease.name}
-                </NaviBar>
-            
-                <div className="container my-3">
-                    {status &&
-                        <div className="alert alert-success"><AmaranthIcon icon={aiCheck} /> {status}</div>
-                    }
-                    <fieldset className="row mb-3">
-                        <div className="col-12">
-                            <h4 className="h5 mb-0">Changelog</h4>
-                            <p className="text-muted mb-0"><small>What's new?</small></p>
-                        </div>
-                        <div className="col-12 mt-3">
-                            <div className="card">
-                                <div className="card-body ps-editor changelog-content">
-                                    <textarea
-                                        autoFocus
-                                        value={curRelease.changelog}
-                                        onChange={(value) => setChangelog(value)}
-                                        className="form-control"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
+  return (
+    <Admin>
+      <form onSubmit={handleSubmit}>
+        <NaviBar
+          back="/admin/releases"
+          actions={
+            <button className="btn btn-primary btn-sm" type="submit"><AmaranthIcon icon={aiFloppyDisc} /> Save</button>
+          }
+        >
+          {curRelease.name}
+        </NaviBar>
+      
+        <div className="container my-3">
+          {status &&
+            <div className="alert alert-success"><AmaranthIcon icon={aiCheck} /> {status}</div>
+          }
+          <fieldset className="row mb-3">
+            <div className="col-12">
+              <h4 className="h5 mb-0">Changelog</h4>
+              <p className="text-muted mb-0"><small>What's new?</small></p>
+            </div>
+            <div className="col-12 mt-3">
+              <div className="card">
+                <div className="card-body p-0 changelog-content">
+                  <textarea
+                    autoFocus
+                    value={curRelease.changelog}
+                    onChange={(event) => setCurRelease((curRelease) => ({ ...curRelease, changelog: event.target.value }))}
+                    className="form-control changelog-control"
+                  />
                 </div>
-            </form>
-        </Admin>
-    )
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </form>
+    </Admin>
+  )
 }
