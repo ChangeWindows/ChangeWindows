0f08dd4129f4000a5ca2bcefb137111d3d32517f
Yannick <yannick.franssen@outlook.com>
2022-02-16 23:43:27 +0100

    Cleanup changelog style, improve timeline wrapping

--- resources/js/Components/Timeline/Launch.jsx
+++ resources/js/Components/Timeline/Launch.jsx
@@ -5,26 +5,25 @@ import PlatformIcon from '../Platforms/PlatformIcon';
 import clsx from 'clsx';
 
 export default function Launch({ platform, version, url = null, overview = false, sidebar = false }) {
-    const Component = useMemo(() => (url ? InertiaLink : 'div'), ['url']);
-    const mainProps = useMemo(() => ({ href: url }), ['url']);
+  const Component = useMemo(() => (url ? InertiaLink : 'div'), ['url']);
+  const mainProps = useMemo(() => ({ href: url }), ['url']);
 
-    return (
-        <Component {...mainProps} className="event text-white" style={{ background: platform.color}}>
-            <div className="icon">
-                <PlatformIcon platform={platform} />
-            </div>
-            <div className="message">Version <span className="fw-bold">{version}</span> is now flighting</div>
-            <div className={
-                clsx(
-                    'version',
-                    {
-                        'd-none': overview || sidebar,
-                        'd-sm-block': overview && !sidebar || !sidebar
-                    }
-                )
-            }>
-                {version}
-            </div>
-        </Component>
-    );
+  return (
+    <Component {...mainProps} className={clsx('event text-white', { 'full-timeline': !sidebar })} style={{ background: platform.color }}>
+      <div className="icon">
+        <PlatformIcon platform={platform} />
+      </div>
+      <div className="message">Version <span className="fw-bold">{version}</span> is now flighting</div>
+      <div className={
+        clsx(
+          'version',
+          {
+            'd-none': overview || sidebar
+          }
+        )
+      }>
+        {version}
+      </div>
+    </Component>
+  );
 };
--- resources/js/Components/Timeline/Timeline.jsx
+++ resources/js/Components/Timeline/Timeline.jsx
@@ -3,24 +3,24 @@ import React, { useMemo } from 'react';
 import { format, isToday, isYesterday } from 'date-fns';
 
 export default function Timeline({ date, children }) {
-    const formatedDate = useMemo(() => {
-        if (isToday(date)) {
-            return 'Today';
-        } else if (isYesterday(date)) {
-            return 'Yesterday';
-        } else {
-            return format(date, 'd MMMM yyyy');
-        };
-    }, [date]);
+  const formatedDate = useMemo(() => {
+    if (isToday(date)) {
+      return 'Today';
+    } else if (isYesterday(date)) {
+      return 'Yesterday';
+    } else {
+      return format(date, 'd MMMM yyyy');
+    };
+  }, [date]);
 
-    return (
-        <>
-            <div className="col-12 titel">
-                <h3 className="h6 text-primary">{formatedDate}</h3>
-            </div>
-            <div className="col-12 timeline">
-                {children}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="col-12 titel">
+        <h3 className="h6 text-primary">{formatedDate}</h3>
+      </div>
+      <div className="col-12 timeline">
+        {children}
+      </div>
+    </>
+  );
 };
