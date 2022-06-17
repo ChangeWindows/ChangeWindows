46d0bbe0ad340c076e6e79bf704cf973f4badd3c
Yannick <yannick.franssen@outlook.com>
2022-06-17 23:32:56 +0200

    Fix react-dom import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { InertiaProgress } from "@inertiajs/progress";
 import { App } from "@inertiajs/inertia-react";
 
