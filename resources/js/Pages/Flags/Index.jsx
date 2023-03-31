5ce07407ee7ef7b3b4944991410be4b82b42eba0
Yannick <yannick.franssen@outlook.com>
2023-03-31 21:31:06 +0200

    Fix undefined icons causing crashes

--- resources/js/Pages/Flags/Index.jsx
+++ resources/js/Pages/Flags/Index.jsx
@@ -10,7 +10,6 @@ import FlagTimeline from "@/Components/_FlagsTimeline";
 import AmaranthIcon, {
   aiBarsStaggered,
   aiCircleInfo,
-  aiClockRotateLeft,
   aiStar,
   aiTrashCan,
 } from "@changewindows/amaranth";
