ef98dd04d9b6bb797eff564144987b5ba242eaa2
Yannick <yannick.franssen@outlook.com>
2023-08-23 23:59:04 +0200

    Reorganize releases page

--- resources/js/Pages/Admin/Releases/Index.jsx
+++ resources/js/Pages/Admin/Releases/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import { Link } from '@inertiajs/react';
+import { Link } from "@inertiajs/react";
 
 import Admin from "@/Layouts/Admin";
 import NaviBar from "@/Components/NaviBar";
@@ -9,48 +9,90 @@ import AmaranthIcon, { aiPlus } from "@studio384/amaranth";
 import PlatformCard from "./_PlatformCard";
 
 export default function Index({ can, releases, status }) {
-  const releasesActive = useMemo(() => {
-    const _releases = JSON.parse(JSON.stringify(releases));
+  const now = new Date().getTime();
 
-    Object.keys(_releases).map((platform) => {
-      _releases[platform].releases = _releases[
-        platform
-      ].releases.filter((release) => release.channels.length !== 0);
+  const _releases = useMemo(() => {
+    let acc = {};
 
-      if (_releases[platform].releases.length === 0) {
-        delete _releases[platform];
-      }
+    Object.keys(releases).map((platform) => {
+      releases[platform].releases.map((_release) => {
+        const previewStart = new Date(_release.start_preview).getTime();
+        const publicStart = new Date(_release.start_public).getTime();
+        const extendedStart = new Date(_release.start_extended).getTime();
+        const ltsStart = new Date(_release.start_lts).getTime();
+        const ltsEnd = new Date(_release.end_lts).getTime();
+
+        if (previewStart < now && (publicStart > now || !publicStart && _release.ongoing)) {
+          if (!acc?.dev) {
+            acc.dev = {};
+          }
+
+          if (!acc.dev?.[platform]) {
+            acc.dev[platform] = {
+              releases: [],
+              platform: releases[platform].platform
+            };
+          }
+
+          acc.dev[platform].releases.push(_release);
+        } else if (publicStart < now && (ltsStart > now || !ltsStart && _release.ongoing || extendedStart > now || !extendedStart && _release.ongoing)) {
+          if (!acc?.public) {
+            acc.public = {};
+          }
+
+          if (!acc.public?.[platform]) {
+            acc.public[platform] = {
+              releases: [],
+              platform: releases[platform].platform
+            };
+          }
+
+          acc.public[platform].releases.push(_release);
+        } else if (ltsStart < now && (ltsEnd > now || !ltsEnd && _release.ongoing)) {
+          if (!acc?.lts) {
+            acc.lts = {};
+          }
+
+          if (!acc.lts?.[platform]) {
+            acc.lts[platform] = {
+              releases: [],
+              platform: releases[platform].platform
+            };
+          }
+
+          acc.lts[platform].releases.push(_release);
+        } else {
+          if (!acc?.legacy) {
+            acc.legacy = {};
+          }
+
+          if (!acc.legacy?.[platform]) {
+            acc.legacy[platform] = {
+              releases: [],
+              platform: releases[platform].platform
+            };
+          }
+
+          acc.legacy[platform].releases.push(_release);
+        }
+      });
     });
 
-    return _releases;
-  }, [releases]);
-
-  const releasesInactive = useMemo(() => {
-    const _releases = JSON.parse(JSON.stringify(releases));
-
-    Object.keys(_releases).map((platform) => {
-      _releases[platform].releases = _releases[
-        platform
-      ].releases.filter((release) => release.channels.length === 0);
-
-      if (_releases[platform].releases.length === 0) {
-        delete _releases[platform];
-      }
-    });
-
-    return _releases;
-  }, [releases]);
+    return acc;
+  }, []);
 
   return (
     <Admin>
       <NaviBar
-        actions={can.releases.create &&
-          <Link
-            href={route("admin.releases.create")}
-            className="btn btn-primary btn-sm"
-          >
-            <AmaranthIcon icon={aiPlus} /> New
-          </Link>
+        actions={
+          can.releases.create && (
+            <Link
+              href={route("admin.releases.create")}
+              className="btn btn-primary btn-sm"
+            >
+              <AmaranthIcon icon={aiPlus} /> New
+            </Link>
+          )
         }
       >
         Releases
@@ -60,14 +102,38 @@ export default function Index({ can, releases, status }) {
         <Status status={status} />
         <div className="row g-1">
           <div className="col-12 titel">
-            <h3 className="h6">Active</h3>
+            <h3 className="h6">Active development</h3>
           </div>
           <div className="col-12 timeline">
-            {Object.keys(releasesActive).map((platform) => (
+            {Object.keys(_releases.dev).map((platform) => (
               <PlatformCard
                 key={platform}
                 can={can}
-                platform={releasesActive[platform]}
+                platform={_releases.dev[platform]}
+              />
+            ))}
+          </div>
+          <div className="col-12 titel">
+            <h3 className="h6">Public</h3>
+          </div>
+          <div className="col-12 timeline">
+            {Object.keys(_releases.public).map((platform) => (
+              <PlatformCard
+                key={platform}
+                can={can}
+                platform={_releases.public[platform]}
+              />
+            ))}
+          </div>
+          <div className="col-12 titel">
+            <h3 className="h6">LTS</h3>
+          </div>
+          <div className="col-12 timeline">
+            {Object.keys(_releases.lts).map((platform) => (
+              <PlatformCard
+                key={platform}
+                can={can}
+                platform={_releases.lts[platform]}
               />
             ))}
           </div>
@@ -75,11 +141,11 @@ export default function Index({ can, releases, status }) {
             <h3 className="h6">Legacy</h3>
           </div>
           <div className="col-12 timeline">
-            {Object.keys(releasesInactive).map((platform) => (
+            {Object.keys(_releases.legacy).map((platform) => (
               <PlatformCard
                 key={platform}
                 can={can}
-                platform={releasesInactive[platform]}
+                platform={_releases.legacy[platform]}
               />
             ))}
           </div>
